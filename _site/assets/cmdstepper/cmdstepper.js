68998e5a267ba2e45b3d25f435e40ce9c407ebe0
John <john@tedspectrum.com>
2019-11-09 13:04:15 +0000

    added variable step length to stepper, delay function

--- _site/assets/cmdstepper/cmdstepper.js
+++ _site/assets/cmdstepper/cmdstepper.js
@@ -1,21 +1,62 @@
 function CmdStepper(stepLength) {
-  var boundStep,
-    cmdIndex = 0,
+  var cmdIndex = 0,
     cmds = [],
     isPaused = false,
     isStepping = false,
     timeoutID = 0,
-    ms = stepLength;
-  this.current = function() {
+    defaultms = (stepLength) ? stepLength : 100;
+  function boundStep() {
+    let ms = 0,
+      cmd = cmds[cmdIndex];
+    // cmd should have do(), isDone(), reset() functions and ms property
+    cmd.do && cmd.do();
+    if (cmd.isDone && !cmd.isDone()) {
+      // cmd that doesn't have isDone is done once only.
+      // cmd has more steps to do.
+      ms = (cmd.hasOwnProperty('ms')) ? cmd.ms : defaultms;
+      if (ms > 0) {
+        timeoutID = window.setTimeout(boundStep, ms);
+      } else {
+        timeoutID = 0;
+        boundStep();
+      }
+    } else if (cmdIndex < (cmds.length - 1)) {
+      // cmd has no more steps to do, do next if it exists
+      cmdIndex++;
+      boundStep();
+    } else {
+      // this cmd is done, there are no more.
+      timeoutID = 0;
+      isStepping = false;
+    }
+  }
+  function CmdWait (delayMs) {
+    var i = 0;
+    this.do = function () {
+      i++;
+    }
+    this.isDone = function () {
+      return (i > 1);
+    }
+    this.ms = delayMs;
+  }
+  this.current = function () {
     return cmds[cmdIndex];
   }
+  this.delay = function (delayMs) {
+    return new CmdWait(delayMs);
+  }
+  this.getDefaultMs = function () {
+    return defaultms;
+  }
+  this.isStepping = function () {
+    return isStepping;
+  }
   this.pause = function () {
-    if(isStepping) { 
-      if(!isPaused) {
+    if (isStepping) {
+      if (!isPaused) {
         isPaused = true;
-        if (timeoutID !== 0) {
-          window.clearTimeout(timeoutID);
-        }
+        (timeoutID !== 0) && window.clearTimeout(timeoutID);
       } else {
         isPaused = false;
         boundStep();
@@ -24,30 +65,24 @@ function CmdStepper(stepLength) {
   }
   this.reset = function () {
     cmdIndex = 0;
-    cmds.forEach(function (v) { v.reset(); });
+    cmds.forEach(function (v) { v.reset && v.reset(); });
     isPaused = false;
     isStepping = false;
-    if (timeoutID !== 0) {
-      window.clearTimeout(timeoutID);
-    }
+    (timeoutID !== 0) && window.clearTimeout(timeoutID);
   }
-  this.start = function (newCmds) {
-    cmds = newCmds;
-    this.reset();
-    isStepping = true;
-    boundStep();
+  this.setDefaultMs = function(newMs) {
+    defaultms = newMs;
   }
-  this.step = function () {
-    let cmd = cmds[cmdIndex];
-    cmd.do();
-    if (!cmd.isDone()) {
-      timeoutID = window.setTimeout(boundStep, ms);
-    } else if (cmdIndex < (cmds.length - 1)) {
-      cmdIndex++;
+  this.start = function (newCmds, newMs) {
+    if (!isStepping) {
+      if (newCmds) { cmds = newCmds; };
+      if (newMs) { defaultms = newMs; };
+      this.reset();
+      isStepping = true;
       boundStep();
-    } else {
-      isStepping = false;
     }
   }
-  boundStep = this.step.bind(this);
+  this.stop = function() {
+    (isStepping === true) && this.reset();
+  }
 }
