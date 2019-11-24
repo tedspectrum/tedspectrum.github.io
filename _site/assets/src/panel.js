c28c51d4347ba616435fb744a0ca6cda79c1c039
John <john@tedspectrum.com>
2019-11-24 15:03:07 +0000

    left and right menus

--- _site/assets/src/panel.js
+++ _site/assets/src/panel.js
@@ -2,7 +2,7 @@ Vue.component('panel', {
   template: `
 <div data-id='panel-template'>
   <transition name='overlay'>
-    <div v-if='active' class='overlay' @click='$emit("panel-close")'></div>
+    <div v-if='active && overlay' class='overlay' @click='$emit("close")'></div>
   </transition>
   <transition :name='transition'>
     <div v-if='active' class='panel' :class='classes'>
@@ -11,5 +11,5 @@ Vue.component('panel', {
   </transition>
 </div>
 `,
-  props: ["classes", "transition", "active"]
+  props: ["classes", "transition", "active", "overlay"]
 });
