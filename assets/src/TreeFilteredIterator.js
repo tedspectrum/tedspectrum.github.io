0470d2d6ee6138b8102ad3b0e89b79afd01cfe89
John <john@tedspectrum.com>
2019-12-26 23:21:30 +0000

    more code tidy in treefilterediterator

--- assets/src/TreeFilteredIterator.js
+++ assets/src/TreeFilteredIterator.js
@@ -1,14 +1,17 @@
 function TreeFilteredIterator(tree, filterFn) {
-  var ancestorIndexes = [],
-    ancestorValues = [],
-    current = null,
-    isDone = false,
+  const DONE_VALUE = { 'done': true };
+  var currentValue = null, // object
+    hasNext = true,
+    nodeStackLength = 0,
+    nodeStackIndexes = [],
+    nodeStackValues = [],
     searchIndex = 0,
-    searchValue = null;
+    searchValue = null, // object
+    stepCount = 0;
   if (typeof filterFn === "function") {
     this.filter = filterFn;
   } else {
-    this.filter = function (/* node, index of node in ancestor, tree */) {
+    this.filter = function (/* node, number of steps to node in iteration, iterator */) {
       // return true to include node in iterator output
       return true;
     }
@@ -17,36 +20,37 @@ function TreeFilteredIterator(tree, filterFn) {
     // there are no objects or variables in step.
     this.step();
     // iterator protocol requires return of an object
-    return isDone ? { 'done': true } : { 'done': isDone, 'value': current };
+    return (hasNext) ? { 'done': false, 'value': currentValue } : DONE_VALUE;
   };
   this.step = function () {
-    isDone = true;
-    if (current === null) {
-      // first step
-      if (this.filter(tree, 0, tree)) {
-        current = tree;
-        isDone = false;
+    hasNext = false;
+    if (stepCount === 0) {
+      if (this.filter(tree, stepCount, this)) {
+        currentValue = tree;
+        hasNext = true;
       }
-    } else {
-      if (current.contents && current.contents.length > 0) {
-        ancestorIndexes.push(-1);
-        ancestorValues.push(current);
-      }
-      while (ancestorValues.length !== 0 && isDone) {
-        searchIndex = ancestorIndexes.pop() + 1;
-        searchValue = ancestorValues.pop();
-        while (searchIndex < searchValue.contents.length) {
-          if (this.filter(searchValue.contents[searchIndex], searchIndex, tree)) {
-            ancestorIndexes.push(searchIndex);
-            ancestorValues.push(searchValue);
-            current = searchValue.contents[searchIndex];
-            isDone = false;
-            break;
-          }
+    } else if (currentValue.contents) {
+      nodeStackIndexes[nodeStackLength] = -1;
+      nodeStackValues[nodeStackLength] = currentValue;
+      nodeStackLength++;
+    }
+    while (nodeStackLength > 0 && !hasNext) {
+      nodeStackLength--;
+      searchIndex = nodeStackIndexes[nodeStackLength] + 1;
+      searchValue = nodeStackValues[nodeStackLength];
+      while (searchIndex < searchValue.contents.length && !hasNext) {
+        if (this.filter(searchValue.contents[searchIndex], stepCount, this)) {
+          nodeStackIndexes[nodeStackLength] = searchIndex;
+          nodeStackValues[nodeStackLength] = searchValue;
+          nodeStackLength++;
+          currentValue = searchValue.contents[searchIndex];
+          hasNext = true;
+        } else {
           searchIndex++;
         }
       }
     }
+    stepCount++;
   }
   this[Symbol.iterator] = function () { return this; };
 }
