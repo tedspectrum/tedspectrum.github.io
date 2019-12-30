5a575ca2bc28972c5c13828ec3e92c02d301fd0e
John <john@tedspectrum.com>
2019-12-30 23:31:45 +0000

    added start panel

---
+++ assets/canvasimages/app.js
@@ -0,0 +1,128 @@
+const app = new Vue({
+  el: '#app',
+  data: function () {
+    let d = {
+      changed: false,
+      core: {},
+      controlButton: {
+        'disabled': true,
+        'activeMsg': '',
+        'msgLoading': 'Loading...',
+        'msgReady': 'Start'
+      },
+      kb: {},
+      showMain: false,
+      showStartPanel: true,
+      tileMapViewer: {},
+      viewHeight: 240,
+      viewWidth: 320,
+      moveRate: 8,
+      mousepresses: {
+        up: false,
+        down: false,
+        left: false,
+        right: false
+      },
+      tileMapURL: '/assets/canvasimages/tilemap.json',
+      tileMap: {},
+      touches: {
+        up: false,
+        down: false,
+        left: false,
+        right: false
+      }
+    };
+    return d;
+  },
+  mounted: function () {
+    let myself = this;
+    this.core = new Core();
+    this.kb = new KeyHandler(window, {
+      'up': 'w',
+      'down': 's',
+      'left': 'a',
+      'right': 'd'
+    });
+    this.controlButton.activeMsg = this.controlButton.msgLoading;
+    axios.get(this.tileMapURL).then(function (res) {
+      myself.$set(myself, 'tileMap', res.data);
+      myself.controlButton.activeMsg = myself.controlButton.msgReady;
+      myself.controlButton.disabled = false;
+    });
+  },
+  methods: {
+    onEruda: function () {
+      this.core.eruda();
+    },
+    onFullScreen: function () {
+      this.core.toggleFullScreen(this.$el);
+    },
+    onMouseDown: function (buttonId) {
+      if (this.mousepresses[buttonId] === false) {
+        this.mousepresses[buttonId] = true;
+      }
+    },
+    onMouseUp: function (buttonId) {
+      if (this.mousepresses[buttonId] === true) {
+        this.mousepresses[buttonId] = false;
+      }
+    },
+    onTouchStart: function (buttonId) {
+      if (this.touches[buttonId] === false) {
+        this.touches[buttonId] = true;
+      }
+    },
+    onTouchEnd: function (buttonId) {
+      if (this.touches[buttonId] === true) {
+        this.touches[buttonId] = false;
+      }
+    },
+    onTouchCancel: function (buttonId) {
+      if (this.touches[buttonId] === true) {
+        this.touches[buttonId] = false;
+      }
+    },
+    run: function () {
+      this.tileMapViewer = new TileMapViewer(
+        {
+          map: this.tileMap,
+          spriteSheet: this.$refs.tiles,
+          tilesize: 32,
+          outputEl: this.$refs.layer1
+        }
+      );
+      this.kb.start();
+      this.changed = true;
+      this.boundUpdate = this.update.bind(this);
+      this.showStartPanel = false;
+      this.boundUpdate(0);
+    },
+    update: function (timestamp) {
+      if (this.touches.left) { this.move(-1, 0); }
+      if (this.touches.right) { this.move(1, 0); }
+      if (this.touches.up) { this.move(0, -1); }
+      if (this.touches.down) { this.move(0, 1); }
+      if (!this.changed) {
+        if (this.mousepresses.left) { this.move(-1, 0); }
+        if (this.mousepresses.right) { this.move(1, 0); }
+        if (this.mousepresses.up) { this.move(0, -1); }
+        if (this.mousepresses.down) { this.move(0, 1); }
+      }
+      if (!this.changed) {
+        if (this.kb.key.left) { this.move(-1, 0); }
+        if (this.kb.key.right) { this.move(1, 0); }
+        if (this.kb.key.up) { this.move(0, -1); }
+        if (this.kb.key.down) { this.move(0, 1); }
+      }
+      if (this.changed === true) {
+        this.changed = false;
+        this.tileMapViewer.update();
+      }
+      window.requestAnimationFrame(this.boundUpdate);
+    },
+    move: function (moveX, moveY) {
+      this.tileMapViewer.changeXY(moveX * this.moveRate, moveY * this.moveRate);
+      this.changed = true;
+    }
+  }
+});
