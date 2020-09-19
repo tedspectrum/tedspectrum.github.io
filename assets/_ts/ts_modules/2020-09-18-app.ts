d7bfd47db18f30e9aa2e011c1345f268f47af473
tedspectrum <tedspectrum@users.noreply.github.com>
2020-09-19 19:36:01 +0100

    using typescript

--- assets/_ts/ts_modules/2020-09-18-app.ts
+++ assets/_ts/ts_modules/2020-09-18-app.ts
@@ -1,75 +1,190 @@
-const appTemplate = /*html*/`
-<div id="postapp" class="app-container" v-cloak>
+const PanelComponent = {
+  name: 'teds-panel',
+  template: /*html*/`
+    <div>
+      <transition :name="overlay_transition">
+        <div v-show="model.active && overlay" class="overlay"
+          @click="$emit('panel-activate', model, false)"></div>
+      </transition>
+      <transition :name="transition">
+        <div v-show="model.active" class="panel" :class="classes">
+          <slot></slot>
+        </div>
+      </transition>
+    </div>`,
+  props: {
+    classes: {
+      type: String,
+      default: "panel-fullheightleft"
+    },
+    model: {
+      type: Object,
+      required: true
+    },
+    overlay: {
+      default: true
+    },
+    overlay_transition: {
+      type: String,
+      default: "fadeinout"
+    },
+    transition: {
+      type: String,
+      default: "slideright"
+    }
+  }
+};
+const AppTemplate = /*html*/`
+<div id="postapp" class="app-container panel-container" v-cloak>
   <div v-show="false">
   <!-- cache, give elements ref="" to reference in methods -->
   </div>
   <div class="app layout-rows">
     <div class="app-header">
-      <h1>
-      <svg viewBox="0 0 20 20" fill="currentColor" class="menu--svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-      {{ title }}</h1>
-    </div>
-    <div>
-      <p>{{ description }}</p>
-      <button v-on:click="onThemeToggle">
-        {{ themeState ? "Theme Off" : "Theme On" }}
+      <button
+        @click="onPanelSet(mainmenu, true)">
+        <svg viewBox="0 0 20 20" fill="currentColor" class="menu--svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
+      <h1 class="inline">{{ app.content.title }}</h1>
+    </div>
+    <teds-panel 
+      :model="mainmenu" 
+      @panel-activate="onPanelSet">
+      <div class="app-header">
+        <h1>{{ mainmenu.content.title }}</h1>
+      </div>
+      <button @click="onThemeToggle(theme)">
+        {{ theme.active ? theme.content.remove : theme.content.add }}
+      </button>
+      <button @click="onErudaToggle(eruda)">
+        {{ eruda.active ? eruda.content.remove : eruda.content.add }}
+      </button>
+    </teds-panel>
+    <div>
+      <p>{{ app.content.description }}</p>
     </div>
     <div class="app-footer">
-      <span>By TedSpectrum</span>
+      <span>{{ app.content.author }}</span>
     </div>
   </div>
 </div>
-`
-function addStyle(id: string, content: string) {
-  if (document.getElementById(id) === null) {
-    let sheet = document.createElement('style');
-    if (sheet !== null && document.head) {
-      sheet.id = id;
-      sheet.innerHTML = content;
-      document.head.appendChild(sheet);
+`;
+// Class Core
+declare let eruda: any;
+class Core {
+  static addStyle(id: string, content: string): void {
+    if (document.getElementById(id) === null) {
+      let el = document.createElement('style');
+      if (el !== null && document.head) {
+        el.id = id;
+        el.innerHTML = content;
+        document.head.appendChild(el);
+      }
     }
   }
-}
-function removeStyle(id: string) {
-  let sheet = document.getElementById(id);
-  if (sheet !== null && sheet.parentNode !== null) {
-    sheet.parentNode.removeChild(sheet);
+  static addEruda(): void {
+    if (document.getElementById('eruda-script') === null) {
+      let el = document.createElement('script');
+      if (el !== null && document.body) {
+        el.id = 'eruda-script';
+        el.src = '//cdn.jsdelivr.net/npm/eruda';
+        el.onload = function () {
+          eruda.init();
+        };
+        document.body.appendChild(el);
+      }
+    } else {
+      eruda.init();
+    }
+  }
+  static removeElementById(id: string): void {
+    let el = document.getElementById(id);
+    if (el !== null && el.parentNode !== null) {
+      el.parentNode.removeChild(el);
+    }
+  }
+  static removeEruda(): void {
+    Core.removeElementById('eruda-script');
+    if(eruda) { eruda.destroy(); }
+  }
+  static removeStyle(id: string): void {
+    Core.removeElementById(id);
   }
 }
 export const App = Vue.extend({
-  created() {
-    addStyle('theme', getAppTheme());
+  components: {
+    'teds-panel': PanelComponent
   },
-  data() {
+  created: function () {
+    Core.addStyle('theme', getAppTheme());
+  },
+  data: function () {
     return {
-      title: 'Post app',
-      description: 'playing about',
-      themeState: false,
-      themeUserOptions: {
-        color: 'blue'
-      }
+      app: {
+        content: {
+          author: 'By TedSpectrum',
+          description: 'playing about',
+          title: 'Post app'
+        }
+      },
+      eruda: {
+        active: false,
+        content: {
+          add: 'Start Eruda',
+          remove: 'Remove Eruda'
+        }
+      },
+      mainmenu: {
+        active: false,
+        content: {
+          title: 'Menu'
+        }
+      },
+      theme: {
+        active: false,
+        id: 'user',
+        content: {
+          color: 'blue',
+          add: 'Theme On',
+          remove: 'Theme Off'
+        }
+      },
     };
   },
   methods: {
-    onThemeToggle: function (): void {
-      if (this.themeState) {
-        removeStyle('userTheme');
-        this.themeState = false;
+    onErudaToggle: function (m: { active: boolean, id: string }): void {
+      if (m.active) {
+        Core.removeEruda();
+        m.active = false;
       } else {
-        addStyle('userTheme', getUserTheme(this.themeUserOptions));
-        this.themeState = true;
+        Core.addEruda();
+        m.active = true;
+      }
+    },
+    onPanelSet: function (m: { active: boolean }, val: boolean): void {
+      if (m.active !== val) { m.active = val; };
+    },
+    onThemeToggle: function (m: { active: boolean, id: string, content: object }): void {
+      if (m.active) {
+        Core.removeStyle(m.id);
+        m.active = false;
+      } else {
+        Core.addStyle(m.id, getUserTheme(m.content));
+        m.active = true;
       }
     }
   },
-  template: appTemplate
+  mounted: function () {
+    (this.$el as HTMLElement).focus();
+  },
+  template: AppTemplate
 });
-function getUserTheme(options: { [key: string]: any; }): string {
+function getUserTheme(content: { [key: string]: any; }): string {
   return /*css*/`
   .app {
-    color: ${options.color}
-  }`
-}
+    color: ${content.color}
+  }`;
+};
 function getAppTheme(): string {
   return/*css*/`
   .app {
@@ -92,6 +207,9 @@ function getAppTheme(): string {
     display: grid;
     place-items: center;
   }
+  .inline {
+    display: inline;
+  }
   .layout-rows {
     display: grid;
     grid-template-rows: auto 1fr auto;
@@ -111,5 +229,6 @@ function getAppTheme(): string {
   .menu--svg {
     width: 16px;
     height: 16px;
+    display: block;
   }`
-}
+};
--- assets/src/ts_modules/2020-09-18-app.js
+++ assets/src/ts_modules/2020-09-18-app.js
@@ -1,51 +1,153 @@
-var appTemplate = "\n<div id=\"postapp\" class=\"app-container\" v-cloak>\n  <div v-show=\"false\">\n  <!-- cache, give elements ref=\"\" to reference in methods -->\n  </div>\n  <div class=\"app layout-rows\">\n    <div class=\"app-header\">\n      <h1>\n      <svg viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"menu--svg\"><path fill-rule=\"evenodd\" d=\"M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z\" clip-rule=\"evenodd\"></path></svg>\n      {{ title }}</h1>\n    </div>\n    <div>\n      <p>{{ description }}</p>\n      <button v-on:click=\"onThemeToggle\">\n        {{ themeState ? \"Theme Off\" : \"Theme On\" }}\n      </button>\n    </div>\n    <div class=\"app-footer\">\n      <span>By TedSpectrum</span>\n    </div>\n  </div>\n</div>\n";
-function addStyle(id, content) {
-    if (document.getElementById(id) === null) {
-        var sheet = document.createElement('style');
-        if (sheet !== null && document.head) {
-            sheet.id = id;
-            sheet.innerHTML = content;
-            document.head.appendChild(sheet);
+var PanelComponent = {
+    name: 'teds-panel',
+    template: "\n    <div>\n      <transition :name=\"overlay_transition\">\n        <div v-show=\"model.active && overlay\" class=\"overlay\"\n          @click=\"$emit('panel-activate', model, false)\"></div>\n      </transition>\n      <transition :name=\"transition\">\n        <div v-show=\"model.active\" class=\"panel\" :class=\"classes\">\n          <slot></slot>\n        </div>\n      </transition>\n    </div>",
+    props: {
+        classes: {
+            type: String,
+            default: "panel-fullheightleft"
+        },
+        model: {
+            type: Object,
+            required: true
+        },
+        overlay: {
+            default: true
+        },
+        overlay_transition: {
+            type: String,
+            default: "fadeinout"
+        },
+        transition: {
+            type: String,
+            default: "slideright"
         }
     }
-}
-function removeStyle(id) {
-    var sheet = document.getElementById(id);
-    if (sheet !== null && sheet.parentNode !== null) {
-        sheet.parentNode.removeChild(sheet);
+};
+var AppTemplate = "\n<div id=\"postapp\" class=\"app-container panel-container\" v-cloak>\n  <div v-show=\"false\">\n  <!-- cache, give elements ref=\"\" to reference in methods -->\n  </div>\n  <div class=\"app layout-rows\">\n    <div class=\"app-header\">\n      <button\n        @click=\"onPanelSet(mainmenu, true)\">\n        <svg viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"menu--svg\"><path fill-rule=\"evenodd\" d=\"M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z\" clip-rule=\"evenodd\"></path></svg>\n      </button>\n      <h1 class=\"inline\">{{ app.content.title }}</h1>\n    </div>\n    <teds-panel \n      :model=\"mainmenu\" \n      @panel-activate=\"onPanelSet\">\n      <div class=\"app-header\">\n        <h1>{{ mainmenu.content.title }}</h1>\n      </div>\n      <button @click=\"onThemeToggle(theme)\">\n        {{ theme.active ? theme.content.remove : theme.content.add }}\n      </button>\n      <button @click=\"onErudaToggle(eruda)\">\n        {{ eruda.active ? eruda.content.remove : eruda.content.add }}\n      </button>\n    </teds-panel>\n    <div>\n      <p>{{ app.content.description }}</p>\n    </div>\n    <div class=\"app-footer\">\n      <span>{{ app.content.author }}</span>\n    </div>\n  </div>\n</div>\n";
+var Core = (function () {
+    function Core() {
     }
-}
+    Core.addStyle = function (id, content) {
+        if (document.getElementById(id) === null) {
+            var el = document.createElement('style');
+            if (el !== null && document.head) {
+                el.id = id;
+                el.innerHTML = content;
+                document.head.appendChild(el);
+            }
+        }
+    };
+    Core.addEruda = function () {
+        if (document.getElementById('eruda-script') === null) {
+            var el = document.createElement('script');
+            if (el !== null && document.body) {
+                el.id = 'eruda-script';
+                el.src = '//cdn.jsdelivr.net/npm/eruda';
+                el.onload = function () {
+                    eruda.init();
+                };
+                document.body.appendChild(el);
+            }
+        }
+        else {
+            eruda.init();
+        }
+    };
+    Core.removeElementById = function (id) {
+        var el = document.getElementById(id);
+        if (el !== null && el.parentNode !== null) {
+            el.parentNode.removeChild(el);
+        }
+    };
+    Core.removeEruda = function () {
+        Core.removeElementById('eruda-script');
+        if (eruda) {
+            eruda.destroy();
+        }
+    };
+    Core.removeStyle = function (id) {
+        Core.removeElementById(id);
+    };
+    return Core;
+}());
 export var App = Vue.extend({
+    components: {
+        'teds-panel': PanelComponent
+    },
     created: function () {
-        addStyle('theme', getAppTheme());
+        Core.addStyle('theme', getAppTheme());
     },
     data: function () {
         return {
-            title: 'Post app',
-            description: 'playing about',
-            themeState: false,
-            themeUserOptions: {
-                color: 'blue'
-            }
+            app: {
+                content: {
+                    author: 'By TedSpectrum',
+                    description: 'playing about',
+                    title: 'Post app'
+                }
+            },
+            eruda: {
+                active: false,
+                content: {
+                    add: 'Start Eruda',
+                    remove: 'Remove Eruda'
+                }
+            },
+            mainmenu: {
+                active: false,
+                content: {
+                    title: 'Menu'
+                }
+            },
+            theme: {
+                active: false,
+                id: 'user',
+                content: {
+                    color: 'blue',
+                    add: 'Theme On',
+                    remove: 'Theme Off'
+                }
+            },
         };
     },
     methods: {
-        onThemeToggle: function () {
-            if (this.themeState) {
-                removeStyle('userTheme');
-                this.themeState = false;
+        onErudaToggle: function (m) {
+            if (m.active) {
+                Core.removeEruda();
+                m.active = false;
             }
             else {
-                addStyle('userTheme', getUserTheme(this.themeUserOptions));
-                this.themeState = true;
+                Core.addEruda();
+                m.active = true;
+            }
+        },
+        onPanelSet: function (m, val) {
+            if (m.active !== val) {
+                m.active = val;
+            }
+            ;
+        },
+        onThemeToggle: function (m) {
+            if (m.active) {
+                Core.removeStyle(m.id);
+                m.active = false;
+            }
+            else {
+                Core.addStyle(m.id, getUserTheme(m.content));
+                m.active = true;
             }
         }
     },
-    template: appTemplate
+    mounted: function () {
+        this.$el.focus();
+    },
+    template: AppTemplate
 });
-function getUserTheme(options) {
-    return "\n  .app {\n    color: " + options.color + "\n  }";
+function getUserTheme(content) {
+    return "\n  .app {\n    color: " + content.color + "\n  }";
 }
+;
 function getAppTheme() {
-    return "\n  .app {\n    width: 100%;\n    overflow: hidden;\n    background-color: lightgray;\n  }\n  .v-cloak {\n    display: none;\n  }\n  .app-header, .app-footer {\n    background-color: gray;\n    color: white;\n    padding-left: 0.5rem;\n  }\n  .bordered {\n    border: 1px solid black;\n  }\n  .centered {\n    display: grid;\n    place-items: center;\n  }\n  .layout-rows {\n    display: grid;\n    grid-template-rows: auto 1fr auto;\n  }\n  .layout-columns {\n    display: grid;\n    grid-template-columns: auto 1fr auto;\n  }\n  .column-width {\n    width: 12rem;\n  }\n  .app-body-content {\n    width: 50%;\n    height: 50%;\n    overflow: hidden;\n  }\n  .menu--svg {\n    width: 16px;\n    height: 16px;\n  }";
+    return "\n  .app {\n    width: 100%;\n    overflow: hidden;\n    background-color: lightgray;\n  }\n  .v-cloak {\n    display: none;\n  }\n  .app-header, .app-footer {\n    background-color: gray;\n    color: white;\n    padding-left: 0.5rem;\n  }\n  .bordered {\n    border: 1px solid black;\n  }\n  .centered {\n    display: grid;\n    place-items: center;\n  }\n  .inline {\n    display: inline;\n  }\n  .layout-rows {\n    display: grid;\n    grid-template-rows: auto 1fr auto;\n  }\n  .layout-columns {\n    display: grid;\n    grid-template-columns: auto 1fr auto;\n  }\n  .column-width {\n    width: 12rem;\n  }\n  .app-body-content {\n    width: 50%;\n    height: 50%;\n    overflow: hidden;\n  }\n  .menu--svg {\n    width: 16px;\n    height: 16px;\n    display: block;\n  }";
 }
+;
